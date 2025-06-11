e01a7ad5f9d6e7a326d0814c524d216c44580309
Mohammad Waris <mdtech9900@gmail.com>
2025-06-11 12:23:34 +0530

    fix

--- server/api/server.js
+++ server/api/server.js
@@ -21,6 +21,7 @@ const CONNECTED_ACCOUNT_ID = 'acct_1RSCgTRhwk3BcY4W';
 app.get('/',async (req,res)=>{
   return res.json({message:"Hello from backend"})
 })
+
 app.post('/create-checkout-session', async (req, res) => {
   
   
